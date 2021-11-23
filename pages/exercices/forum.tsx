6594a3a9d5e3d945d4262bbf2b38eac637fd8975
Vincent Trélat <vincent.trelat@free.fr>
2021-11-23 16:10:14 +0100

    updates

--- pages/exercices/forum.tsx
+++ pages/exercices/forum.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import Message from '../components/Message'
+import MessageList from '../components/MessageList'
 import MessageForm from '../components/MessageForm'
 
 const Forum: NextPage = () => {
@@ -13,23 +13,7 @@ const Forum: NextPage = () => {
         <div className="container">
           <div className="row">
             <div id="messageField" className="col">
-              <Message body="Salut" author="VilavaneLy" type="incoming" date={new Date} />
-              <Message body="Non, tu peux m'envoyer ton projet de software engineering stp ?" author="VTrelat" type="sent" date={new Date} />
-              <Message body="Non" author="VilavaneLy" type="incoming" date={new Date} />
-              <Message body="Je vais le dire à PEM" author="VTrelat" type="sent" date={new Date} />
-              <Message body="Genre maintenant" author="VTrelat" type="sent" date={new Date} />
-              <Message body="Noooon ! Bon ok je te passe tout" author="VilavaneLy" type="incoming" date={new Date} />
-              <Message body="Je veux 300€ en liquide aussi" author="VTrelat" type="sent" date={new Date} />
-              <Message body="Ok 🥲" author="VilavaneLy" type="incoming" date={new Date} />
-              <Message body="Je te fais un Lydia, passe ton num" author="VilavaneLy" type="incoming" date={new Date} />
-              <Message body="0768207201" author="VTrelat" type="sent" date={new Date} />
-              <Message body="C'est bon" author="VilavaneLy" type="incoming" date={new Date} />
-              <Message body="J'ai reçu que 280" author="VTrelat" type="sent" date={new Date} />
-              <Message body="J'avais que ça 😅" author="VilavaneLy" type="incoming" date={new Date} />
-              <Message body="Ok j'envoie un message à PEM" author="VTrelat" type="sent" date={new Date} />
-              <Message body="Je te passe 20€ en liquide demain !!" author="VilavaneLy" type="incoming" date={new Date} />
-              <Message body="Non paye moi 20 oasis pomme cassis framboise" author="VTrelat" type="sent" date={new Date} />
-              <Message body="Oh non videz pas la machine svp 😅😢" author="PEM" type="incoming" date={new Date} />
+              <MessageList />
             </div>
             <div className="col">
               <MessageForm />
